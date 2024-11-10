34826b02c7becc29f3aa755c920440fa04f43897
Untitled <gnu.imm@outlook.com>
2024-11-11 05:21:15 +0800

    build: export decorator `Dependency`

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     'src/index.ts',
     'src/graph/DiGraph.ts',
     'src/decorators/DependsOn.ts',
+    'src/decorators/Dependency.ts',
     'src/helpers/inversify/getDeps.ts',
   ],
   format: ['esm', 'cjs'],
